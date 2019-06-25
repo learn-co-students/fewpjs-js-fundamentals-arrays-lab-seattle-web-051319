f2738af698b8124e4b0c289cfe4d9f9e0f73751c
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-06-24 17:19:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+
+const prepend = ["Milo", "Otis", "Garfield"];
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+// "Append" means "add to the end"
+// "prepend" means "add to the beginning."
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
